11a6ca197163d73cb743a7d6c2963de7330c482f
sagor <sagor.eee.cuet@gmail.com>
2022-04-29 01:32:12 +0600

    paramMap observable

--- my-app/src/app/department-detail/department-detail.component.ts
+++ my-app/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -9,15 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 export class DepartmentDetailComponent implements OnInit {
 
   public departmentId: any;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('id')||"")
-    if (isNaN(id)) {
-      console.log("id not a number")
-    }else{
-      this.departmentId = id
-    }
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id')||"")
+      if (isNaN(id)) {
+        console.log("id not a number")
+      }else{
+        this.departmentId = id
+      }
+    })
+  }
+
+  goPrevious(){
+    let previous = this.departmentId-1
+    this.router.navigate(["/dept", previous])
+  }
+
+  goNext(){
+    let next = this.departmentId+1
+    this.router.navigate(["/dept", next])
   }
 
 }
